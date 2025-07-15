b52759062b856d3ef6d0ec2c1545aabab70a9a82
dhgb2k5 <dhgb2k5@gmail.com>
2025-07-15 17:25:24 +0700

    Fix Navbar + Banner

---
+++ src/components/HomePage/HeaderWithBanner.tsx
@@ -0,0 +1,32 @@
+
+import NavBar from "../Navbar/Navbar";
+
+interface HeaderWithBanner {
+  banner: string;
+  title: string;
+}
+
+export default function HeaderWithBanner({ banner, title }: HeaderWithBanner) {
+  return (
+    <header className="relative">
+      {/* Banner */}
+
+      <div className=" inset-0 bg-black/35 mix-blend-multiply" />
+
+      {/* NAVIGATION CARD */}
+      <div>
+        <NavBar />
+        <img src={banner} alt="JP TechLift banner" className="h-[530px] w-full object-cover " />
+      </div>
+
+      {/* TITLE */}
+      <h1
+        className="font-inter pointer-events-none absolute inset-0 z-30 flex items-center justify-center
+                     px-4 mt-12 pt-10 text-center text-3xl font-extrabold uppercase tracking-wide text-white
+                     md:text-5xl"
+      >
+        {title}
+      </h1>
+    </header>
+  );
+}
